c00beea147085b3b1c976c264b6564359df5eb90
Rudi Strahl <strahlr@gmail.com>
2013-04-02 00:59:08 +0500

    Fixed ads, now fade in and out properly, are tappable, and are no longer resetting the frame to the screen top. Case 118.

--- ATidyWorld/AdsViewController.m
+++ ATidyWorld/AdsViewController.m
@@ -12,8 +12,16 @@
 #import <AdSupport/AdSupport.h>
 
 @interface AdsViewController ()
+/** Initialize Apple iAd Bannerview */
 - (void)initIadBannerView;
+/** Initialize Google AdMob Bannerview */
 - (void)initAdMobBannerView;
+/** Fade-in animation for presenting a bannerView */
+- (void)showBanner:(UIView *)banner;
+/** Fade-out animation for removing a bannerView */
+- (void)hideBanner:(UIView *)banner;
+/** Sets the frame of the bannerview */
+- (CGRect)setFrame;
 @end
 
 @implementation AdsViewController
@@ -61,23 +69,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CGRect viewFrame;
-    DLog(@"Screen bounds for ads: %f x %f", [[UIScreen mainScreen] bounds].size.width,[[UIScreen mainScreen] bounds].size.height);
-    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
-    {
-        viewFrame = CGRectMake(0,
-                               [[UIScreen mainScreen] bounds].size.height-90,
-                               [[UIScreen mainScreen] bounds].size.width,
-                               90);
-    }
-    else
-    {
-        viewFrame = CGRectMake(0,
-                               [[UIScreen mainScreen] bounds].size.height-50,
-                               [[UIScreen mainScreen] bounds].size.width,
-                               50);
-    }
-    self.view.frame = viewFrame;
+    self.view.frame = [self setFrame];
     if (USE_IAD)
     {
         [self initIadBannerView];
@@ -93,46 +85,64 @@
 #pragma mark - AdBannerViewDelegate (iAd) Implementation
 - (void)bannerViewDidLoadAd:(ADBannerView *)banner
 {
-    banner.hidden = NO;
+    [self showBanner:self.iAdBannerView];
 }
 
 - (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
 {
-    banner.hidden = YES;
-    
-    // Only request adMob when iAd fails
+    if (!self.iAdBannerView.hidden)
+    {
+        [self hideBanner:self.iAdBannerView];
+    }
+    // Fallback to AdMob Banner
     GADRequest *request = [GADRequest request];
     [self.adMobBannerView loadRequest:request];
-    [self hideBanner:self.iAdBannerView];
-    [self showBanner:self.adMobBannerView];
+
 }
 
 - (BOOL)bannerViewActionShouldBegin:(ADBannerView *)banner willLeaveApplication:(BOOL)willLeave
 {
-    // TODO: Instruct game to halt
     return YES;
 }
 
 - (void)bannerViewActionDidFinish:(ADBannerView *)banner
 {
-    // TODO: Instruct game to continue
+    [self hideBanner:banner];
 }
 
 
 #pragma mark - GADBannerViewDelegate (AdMob) Implementation
-- (void)adViewDidReceiveAd:(GADBannerView *)banner
+- (void)adViewDidReceiveAd:(GADBannerView *)bannerView
 {
     DLog(@"");
-    if ([self.iAdBannerView isHidden])
-    {
-        [self showBanner:banner];
-    }
+    [self showBanner:bannerView];
 }
 
-- (void)adView:(GADBannerView *)banner didFailToReceiveAdWithError:(GADRequestError *)error;
+- (void)adView:(GADBannerView *)bannerView didFailToReceiveAdWithError:(GADRequestError *)error;
 {
     DLog(@"");
-    [self hideBanner:banner];
+    [self hideBanner:bannerView];
+}
+
+- (void)adViewWillPresentScreen:(GADBannerView *)bannerView
+{
+    [self hideBanner:bannerView];
+}
+
+- (void)adViewDidDismissScreen:(GADBannerView *)bannerView
+{
+
+}
+
+- (void)adViewWillDismissScreen:(GADBannerView *)bannerView
+{
+//    self.view.frame = [self setFrame];
+    [self requestAdMob];
+}
+
+- (void)adViewWillLeaveApplication:(GADBannerView *)bannerView
+{
+    
 }
 
 #pragma mark - Advertising Initialization
@@ -145,6 +155,8 @@
         mIadBannerView.requiredContentSizeIdentifiers = [NSSet setWithObjects:ADBannerContentSizeIdentifierPortrait, nil];
         mIadBannerView.currentContentSizeIdentifier = ADBannerContentSizeIdentifierPortrait;
         mIadBannerView.delegate = self;
+        mIadBannerView.alpha = 0.0f;
+        mIadBannerView.hidden = YES;
         CGRect adViewFrame = mIadBannerView.frame;
         mIadBannerView.frame = adViewFrame;
         [self.view addSubview:mIadBannerView];
@@ -158,24 +170,33 @@
         // Create a new bottom banner, will be slided into view
         mAdMobBannerView = [[GADBannerView alloc] initWithAdSize:kGADAdSizeSmartBannerPortrait];
         mAdMobBannerView.adUnitID = ADMOB_PUBLISHER_ID;
-        mAdMobBannerView.hidden = !USE_IAD; // If we are using IAD, then the IAD will be given preference
         mAdMobBannerView.rootViewController = self;
         mAdMobBannerView.delegate = self;
+        mAdMobBannerView.alpha = 0.0f;
+        mAdMobBannerView.hidden = YES;
         [self.view addSubview:mAdMobBannerView];
         [self willRequestAd];
     }
 }
 
+#pragma mark - Banner Animations
 - (void)showBanner:(UIView *)banner
 {
     if (banner &&
         [banner isHidden])
     {
-        [UIView beginAnimations:@"animatedBannerOn" context:nil];
-        self.view.alpha = 1.0f;
-//        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
-        [UIView commitAnimations];
+        self.view.frame = [self setFrame];
         banner.hidden = FALSE;
+        [UIView animateWithDuration:1.0
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseIn
+                         animations:^ {
+                             banner.alpha = 1.0;
+                         }
+                         completion:^(BOOL finished) {
+
+                         }];
+
     }
 }
 
@@ -184,14 +205,39 @@
     if (banner &&
         ![banner isHidden])
     {
-        [UIView beginAnimations:@"animatedBannerOff" context:nil];
-        self.view.alpha = 0.0f;
-//        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
-        [UIView commitAnimations];
-        banner.hidden = TRUE;
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveEaseIn
+                         animations:^ {
+                             banner.alpha = 0.0;
+                         }
+                         completion:^(BOOL finished) {
+                             banner.hidden = YES;
+                         }];
     }
 }
 
+- (CGRect)setFrame
+{
+    CGRect viewFrame;
+    if ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
+    {
+        viewFrame = CGRectMake(0,
+                               [[UIScreen mainScreen] bounds].size.height-90,
+                               [[UIScreen mainScreen] bounds].size.width,
+                               90);
+    }
+    else
+    {
+        viewFrame = CGRectMake(0,
+                               [[UIScreen mainScreen] bounds].size.height-50,
+                               [[UIScreen mainScreen] bounds].size.width,
+                               50);
+    }
+    return viewFrame;
+}
+
+#pragma mark - DEBUGGING METHODS
 // DEBUG ONLY
 - (void)toggleAdMobBannerView
 {
@@ -225,6 +271,7 @@
     }
 }
 
+//#pragma mark - AdMob Methods
 - (void)willRequestAd
 {
     if (!USE_IAD)
@@ -233,7 +280,6 @@
     }
 }
 
-//#pragma mark - AdMob Methods
 - (void)requestAdMob
 {
     // Only request adMob when iAd fails
@@ -245,7 +291,6 @@
                                longitude:location.currentLocation.coordinate.longitude
                                 accuracy:location.currentLocation.horizontalAccuracy];
     }
-    [self showBanner:self.adMobBannerView];
     [self.adMobBannerView loadRequest:request];
 }
 
